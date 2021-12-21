fd7f26eb32e952828f6d033737314c7797b8a277
Lucas Bos <lucas@minox.nl>
2021-12-21 22:36:01 +0100

    Day 21 WIP

--- 21/question2.js
+++ 21/question2.js
@@ -1,7 +1,7 @@
 const memory = {};
 
-const playRound = (data, diceValue, i, throws = 1) => {
-    let { pos1, score1, pos2, score2 } = data;
+const playRound = (data, diceValue, i) => {
+    let { pos1, score1, pos2, score2, throws } = data;
     if (i === 0) {
         pos1 += diceValue;
     } else {
@@ -10,7 +10,10 @@ const playRound = (data, diceValue, i, throws = 1) => {
 
     const key = `${diceValue}|${i}|${throws}|${pos1}|${score1}|${pos2}|${score2}`;
 
-    if (memory[key]) return memory[key];
+    if (memory[key]) {
+        console.log("🧜‍♂️");
+        return memory[key];
+    }
 
     if (throws === 3) {
         if (i === 0) {
@@ -20,16 +23,18 @@ const playRound = (data, diceValue, i, throws = 1) => {
             pos2 = pos2 % 10 || 10;
             score2 += pos2;
         }
-        if (score1 >= 21) return [1, 0];
-        if (score2 >= 21) return [0, 1];
+        throws = 0;
     }
+
+    if (score1 >= 21) return [1, 0];
+    if (score2 >= 21) return [0, 1];
 
     const next = throws === 3 ? (i === 0 ? 1 : 0) : i;
 
     const scores = [
-        playRound({ pos1, score1, pos2, score2 }, 1, next, throws + 1),
-        playRound({ pos1, score1, pos2, score2 }, 2, next, throws + 1),
-        playRound({ pos1, score1, pos2, score2 }, 3, next, throws + 1),
+        playRound({ pos1, score1, pos2, score2, throws: throws + 1 }, 1, next),
+        playRound({ pos1, score1, pos2, score2, throws: throws + 1 }, 2, next),
+        playRound({ pos1, score1, pos2, score2, throws: throws + 1 }, 3, next),
     ];
 
     const result = [
@@ -37,6 +42,7 @@ const playRound = (data, diceValue, i, throws = 1) => {
         scores.reduce((acc, val) => acc + val[1], 0),
     ];
     memory[key] = result;
+    console.log("👢", memory);
     return result;
 };
 
